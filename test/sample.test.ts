435a85d6159bd3cd2e61fcc9185c8d9a8887684b
Vitaliy Potapov <noginsk@rambler.ru>
2024-11-24 13:47:40 +0400

    test step in worker fixture teardown

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,11 +1,13 @@
 import { test as base } from '@playwright/test';
 
-const test = base.extend<{myFixture: void}>({
-  myFixture: async ({}, use) => {
+const test = base.extend<{}, {myWorkerFixture: void}>({
+  myWorkerFixture: [async ({}, use) => {
+    await test.step('worker setup', async () => console.log('worker setup'));
     await use();
-  }
+    await test.step('worker teardown', async () => console.log('worker teardown'));
+  }, { scope: 'worker' }],
 });
 
-test('my test', async ({ page }) => {
+test('my test', async ({ page, myWorkerFixture }) => {
   await page.goto('https://playwright.dev');
 });
